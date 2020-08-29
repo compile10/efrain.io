2189389b9e75301b5f3f875bd50c0409913007b3
compile10 <efrain800@gmail.com>
2020-08-28 22:49:21 -0700

    Updated site

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,31 @@
 import * as React from 'react';
 import { NextPage } from 'next';
+import { Box, Flex, Image, Heading , Divider } from "@chakra-ui/core";
+
 
 const IndexPage: NextPage = () => {
-  return <h1>Hello World</h1>;
+
+  return (
+    <Box>
+    <Flex
+    align="center"
+    wrap={'wrap'}
+    padding={'1rem'}
+    color={'gray.600'}
+    shadow={'md'}
+    flexDirection={'row'}
+  >
+    <Heading px={4}>Home</Heading>
+    <Divider orientation="vertical" />
+    <Heading px={4}>My Work</Heading>
+    <Divider orientation="vertical" />
+    
+    <Heading px={4}>YouTube</Heading>
+  </Flex>
+  <Image src='/images/passion.gif'/>
+  </Box>
+    
+  )
 };
 
 export default IndexPage;
