be242c0f92f13fc5e4fe08765e3658f5354f5293
compile10 <efrain800@gmail.com>
2021-10-02 01:15:02 -0700

    Movement restored

--- cube.ts
+++ cube.ts
@@ -88,8 +88,8 @@ export function renderCube(){
 
         const time = Date.now() * 0.001;
 
-       // points.rotation.x = time * 0.25;
-       // points.rotation.y = time * 0.5;
+       points.rotation.x = time * 0.25;
+       points.rotation.y = time * 0.5;
 
         renderer.render( scene, camera );
 
