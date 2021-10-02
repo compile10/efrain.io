8108f9763eb652ddeceb286345966aa9d1d0137c
compile10 <efrain800@gmail.com>
2021-10-01 17:18:24 -0700

    Added simple navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next'
 import  {useEffect } from 'react';
 import { Box, Heading } from "@chakra-ui/react"
+import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Link } from "@chakra-ui/react"
 
 import * as THREE from "three";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+import { GoMarkGithub } from "react-icons/go";
+import { FaLinkedin, FaMedium } from "react-icons/fa";
 
 
 const Home: NextPage = () => {
@@ -59,10 +64,32 @@ const Home: NextPage = () => {
     animate();
     
   });
+
+
   return (
     <Box position="absolute" width="100%" top={80}>
       <Box display="grid" justifyItems="end" padding={[2, 2, 20]}>
-        <Heading color='white'>Welcome to my website.</Heading>
+        <Box display="grid" justifyItems="center">
+          <Heading color='white' >Welcome to my website.</Heading>
+          <ButtonGroup variant="outline" spacing="3" paddingTop={3}>
+            <Link href={'https://github.com/compile10'} isExternal>
+              <Button colorScheme="white" leftIcon={<GoMarkGithub />} >
+                Github
+              </Button>
+            </Link>
+            <Link href={'https://www.linkedin.com/in/efrain800/'} isExternal>
+              <Button colorScheme="white" leftIcon={<FaLinkedin />} >
+                LinkedIn
+              </Button>
+            </Link>
+            <Link href={'https://efrain800.medium.com/'} isExternal>
+              <Button colorScheme="white" leftIcon={<FaMedium />} >
+                Medium
+              </Button>
+            </Link>
+          </ButtonGroup>
+        </Box>
+
       </Box>
     </Box>
   )
